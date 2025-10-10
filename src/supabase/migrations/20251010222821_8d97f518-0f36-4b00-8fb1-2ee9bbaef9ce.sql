87fa460f79e184f3a72d07c373cafbdf5d68deea
isa /m <sawkjz@gmail.com>
2025-10-10 20:05:18 -0300

    Add files via upload

---
+++ src/supabase/migrations/20251010222821_8d97f518-0f36-4b00-8fb1-2ee9bbaef9ce.sql
@@ -0,0 +1,12 @@
+-- Corrigir o search_path da função update_updated_at_column
+CREATE OR REPLACE FUNCTION update_updated_at_column()
+RETURNS TRIGGER 
+LANGUAGE plpgsql
+SECURITY DEFINER
+SET search_path = public
+AS $$
+BEGIN
+    NEW.updated_at = now();
+    RETURN NEW;
+END;
+$$;
