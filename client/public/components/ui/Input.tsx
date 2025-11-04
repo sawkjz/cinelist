42546c7950865c5316c0c31e6431c749d94e54da
isa /m <sawkjz@gmail.com>
2025-11-04 08:36:45 -0300

    Delete client/public/components directory

--- client/public/components/ui/Input.tsx
+++
@@ -1,5 +0,0 @@
-import { InputHTMLAttributes } from 'react'
-
-export function Input(props: InputHTMLAttributes<HTMLInputElement>) {
-  return <input {...props} style={{ width:'100%', padding:8, borderRadius:8, border:'1px solid #e5e7eb' }} />
-}
